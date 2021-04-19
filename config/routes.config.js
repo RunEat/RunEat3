f59372f754e9b57da79378a8bb9a08cb7c2cb639
Ángela Herrador <angelaherrador1991@gmail.com>
2021-04-19 10:46:37 +0200

    Initialition back

---
+++ config/routes.config.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users.controllers');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+
+// User Routes
+router.post('/user/login', usersController.login);
+router.post('/user/signup', usersController.signup);
+// router.post('/auth/upload', authMiddleware.isAuthenticated, usersController.upload);
+router.put('/user/edit', authMiddleware.isAuthenticated, usersController.edit);
+//router.post('/auth/logout', authMiddleware.isAuthenticated, usersController.logout); // Handled from the frontEnd
+router.get('/user/profile', authMiddleware.isAuthenticated, usersController.profile);
+router.post('/user/delete', usersController.delete)
+
+module.exports = router;
---
+++ controllers/users.controllers.js
@@ -0,0 +1,101 @@
+const createError = require('http-errors');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.model');
+
+module.exports.login = (req, res, next) => {
+	const { username, password } = req.body
+
+	User.findOne({ username: username })
+		.then(user => {
+			if (!user) {
+			next(createError(404, { errors: { username: 'username or password are not valid'} }))
+			} else {
+			return user.checkPassword(password)
+				.then(match => {
+				if (!match) {
+					next(createError(404, { errors: { username: 'username or password are not valid'} }))
+				} else {
+					res.json({
+					access_token: jwt.sign(
+						{ id: user._id },
+						process.env.JWT_SECRET || 'JWT Secret - It should be changed',
+						{
+						expiresIn: '1d'
+						}
+					)
+					})
+				}
+				})
+			}
+			})
+		.catch(next)
+}
+
+module.exports.signup = (req, res, next) => {
+	User.findOne({ username: req.body.username })
+		.then(user => {
+			if (user) {
+				next(createError(400, { errors: { username: 'This username is already in use' } }))
+			} else {
+				return User.create(req.body)
+					.then(user => res.status(201).json(user))
+			}
+		})
+		.catch(next)
+}
+
+module.exports.upload = (req, res, next) => {
+
+}
+
+module.exports.edit = (req, res, next) => {
+	console.log('req.body edit', req.body)
+	console.log('req.body.id', req.body.id)
+
+	//req.body.id = req.currentUser;
+
+	console.log('req.currentUser', req.currentUser)
+
+	User.findOne({'_id': req.currentUser})
+		.then(user => {
+			console.log('user', user)
+			if(!user) {
+				next(createError(404));
+				return;
+			}
+			// A bit redundant since the route is protected
+			// if(user.toString() !== req.currentUser.toString()) {
+			// 	next(createError(403));
+			// 	return;
+			// }
+
+			Object.entries(req.body).forEach(([key, value]) => { // For each body element it creates a key value pair
+				user[key] = value;
+			});
+
+			return user.save().then(() => res.json({}));
+		})
+		.catch(next);
+}
+
+// Logout handled from the frontEnd
+// module.exports.logout = (req, res, next) => {
+// 	if(!currentUser) {
+// 		res.send('Logged Out')
+// 	}
+// }
+
+module.exports.profile = (req, res, next) => {
+	User.findById(req.currentUser)
+		.then(user => {
+			if(!user) {
+				next(createERROR(404, 'User not found'))
+			} else {
+				res.json(user)
+			}
+		})
+}
+
+module.exports.delete = (req, res, next) => {
+	
+}
